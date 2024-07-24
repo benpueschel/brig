85c14102d6cba2acf7103ac9f793c894a0d40f37
Ben Püschel <ben.pueschel2004@gmail.com>
2024-07-24 13:17:40 +0200

    feat: add brig-lexer crate

---
+++ brig-lexer/src/lib.rs
@@ -0,0 +1,270 @@
+use std::fmt::Formatter;
+use std::fmt::{self, Display};
+
+use brig_common::Span;
+use brig_diagnostic::{Error, Result};
+
+#[cfg(test)]
+mod test;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Token {
+    pub kind: TokenKind,
+    pub span: Span,
+}
+
+impl Token {
+    pub fn single(kind: TokenKind, pos: usize) -> Self {
+        Self {
+            kind,
+            span: Span::single(pos),
+        }
+    }
+    pub fn with_len(kind: TokenKind, pos: usize, len: usize) -> Self {
+        Self {
+            kind,
+            span: Span::with_len(pos, len),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum TokenKind {
+    Return,
+    Extern,
+    Fn,
+    Let,
+    If,
+    Else,
+    Integer(usize),
+    Identifier(String),
+    Equal,
+    Star,
+    Percent,
+    Slash,
+    Plus,
+    Minus,
+    LeftCaret,
+    RightCaret,
+    ParenOpen,
+    ParenClose,
+    BracketOpen,
+    BracketClose,
+    BraceOpen,
+    BraceClose,
+    Semicolon,
+    Colon,
+    EOF,
+    Invalid,
+}
+
+impl Display for TokenKind {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            TokenKind::Integer(value) => write!(f, "int_lit({})", value),
+            TokenKind::Identifier(name) => write!(f, "ident({})", name),
+            x => write!(f, "{}", x.to_str()),
+        }
+    }
+}
+
+impl TokenKind {
+    pub const fn to_str(&self) -> &str {
+        match self {
+            TokenKind::Extern => "extern",
+            TokenKind::Return => "return",
+            TokenKind::Fn => "fn",
+            TokenKind::Let => "let",
+            TokenKind::If => "if",
+            TokenKind::Else => "else",
+            TokenKind::Integer(_) => "int_lit",
+            TokenKind::Identifier(_) => "ident",
+            TokenKind::Equal => "=",
+            TokenKind::Star => "*",
+            TokenKind::Percent => "%",
+            TokenKind::Slash => "/",
+            TokenKind::Plus => "+",
+            TokenKind::Minus => "-",
+            TokenKind::LeftCaret => "<",
+            TokenKind::RightCaret => ">",
+            TokenKind::ParenOpen => "(",
+            TokenKind::ParenClose => ")",
+            TokenKind::BracketOpen => "[",
+            TokenKind::BracketClose => "]",
+            TokenKind::BraceOpen => "{",
+            TokenKind::BraceClose => "}",
+            TokenKind::Semicolon => ";",
+            TokenKind::Colon => ":",
+            TokenKind::EOF => "EOF",
+            TokenKind::Invalid => "ERROR",
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Lexer {
+    input: String,
+    current_pos: usize,
+}
+
+impl Lexer {
+    pub fn new(input: String) -> Self {
+        Self {
+            input,
+            current_pos: 0,
+        }
+    }
+
+    pub fn tokenize(&mut self) -> Result<Vec<Token>> {
+        let mut tokens = Vec::new();
+        loop {
+            let token = self.next_token()?;
+            if token.kind == TokenKind::EOF {
+                break;
+            }
+            tokens.push(token);
+        }
+        Ok(tokens)
+    }
+
+    pub fn peek(&self) -> Result<Token> {
+        let whitespaces = self.count_whitespaces();
+        let i = self.current_pos + whitespaces;
+        let next = match self.input[i..].chars().next() {
+            None => {
+                return Ok(Token {
+                    kind: TokenKind::EOF,
+                    span: Span::new(i, i),
+                })
+            }
+            Some(x) => x,
+        };
+
+        match next {
+            ';' => Ok(Token::single(TokenKind::Semicolon, i)),
+            ':' => Ok(Token::single(TokenKind::Colon, i)),
+            '=' => Ok(Token::single(TokenKind::Equal, i)),
+            '(' => Ok(Token::single(TokenKind::ParenOpen, i)),
+            ')' => Ok(Token::single(TokenKind::ParenClose, i)),
+            '[' => Ok(Token::single(TokenKind::BracketOpen, i)),
+            ']' => Ok(Token::single(TokenKind::BracketClose, i)),
+            '{' => Ok(Token::single(TokenKind::BraceOpen, i)),
+            '}' => Ok(Token::single(TokenKind::BraceClose, i)),
+            '*' => Ok(Token::single(TokenKind::Star, i)),
+            '%' => Ok(Token::single(TokenKind::Percent, i)),
+            '/' => Ok(Token::single(TokenKind::Slash, i)),
+            '+' => Ok(Token::single(TokenKind::Plus, i)),
+            '-' => Ok(Token::single(TokenKind::Minus, i)),
+            '<' => Ok(Token::single(TokenKind::LeftCaret, i)),
+            '>' => Ok(Token::single(TokenKind::RightCaret, i)),
+
+            c if c.is_ascii_digit() => self.tokenize_number(i),
+            c @ '_' | c if c.is_alphabetic() => self.tokenize_ident(i),
+
+            x => Err(Error::unexpected_symbol(x, i)),
+        }
+    }
+
+    pub fn next_token(&mut self) -> Result<Token> {
+        let token = self.peek()?;
+
+        self.current_pos = token.span.end;
+        Ok(token)
+    }
+
+    pub fn tokenize_ident(&self, pos: usize) -> Result<Token> {
+        // identifiers need to start with an alphabetic ascii char
+        let data = &self.input[pos..];
+        let mut illegal = false;
+        match data.chars().next() {
+            Some(x) if !x.is_alphabetic() => illegal = true,
+            None => return Err(Error::unexpected_eof(pos)),
+            _ => {}
+        };
+
+        let mut i = 0;
+        let mut skip_keyword_val = false;
+        for c in data.chars() {
+            if c == '_' {
+                // NOTE: no keyword contains '_', so we can skip validation
+                skip_keyword_val = true;
+                i += 1;
+                continue;
+            }
+            if !c.is_alphanumeric() {
+                break;
+            }
+            i += 1;
+        }
+
+        if illegal {
+            return Err(Error::invalid_identifier(data, Span::with_len(pos, i)));
+        }
+
+        let identifier = &data[0..i];
+        if skip_keyword_val {
+            return Ok(Token {
+                kind: TokenKind::Identifier(identifier.to_string()),
+                span: Span::with_len(pos, i),
+            });
+        }
+
+        Ok(Token {
+            kind: match identifier {
+                "return" => TokenKind::Return,
+                "let" => TokenKind::Let,
+                "extern" => TokenKind::Extern,
+                "fn" => TokenKind::Fn,
+                "if" => TokenKind::If,
+                "else" => TokenKind::Else,
+                _ => TokenKind::Identifier(identifier.to_string()),
+            },
+            span: Span::with_len(pos, identifier.len()),
+        })
+    }
+
+    fn tokenize_number(&self, start: usize) -> Result<Token> {
+        let data = &self.input[start..];
+        let mut illegal = false;
+
+        match data.chars().next() {
+            Some(x) if !x.is_numeric() => illegal = true,
+            None => return Err(Error::unexpected_eof(start)),
+            _ => {}
+        };
+
+        let mut i = 0;
+        for c in data.chars() {
+            static TERMINATORS: [char; 9] = [';', ':', '=', '(', ')', '[', ']', '{', '}'];
+            if c.is_whitespace() || TERMINATORS.contains(&c) {
+                break;
+            }
+            i += 1;
+        }
+
+        if illegal {
+            return Err(Error::invalid_number(data, Span::with_len(start, i)));
+        }
+
+        let decimal = &data[0..i];
+
+        match decimal.parse() {
+            Ok(value) => Ok(Token {
+                kind: TokenKind::Integer(value),
+                span: Span::with_len(start, i),
+            }),
+            Err(_) => Err(Error::invalid_number(decimal, Span::with_len(start, i))),
+        }
+    }
+
+    fn count_whitespaces(&self) -> usize {
+        let mut i = 0;
+        for c in self.input[self.current_pos..].chars() {
+            if !c.is_whitespace() {
+                break;
+            }
+            i += 1;
+        }
+        i
+    }
+}
---
+++ brig-lexer/src/test.rs
@@ -0,0 +1,66 @@
+use crate::*;
+
+fn test_lexer(input: &str, expected: Vec<Token>) {
+    let mut lexer = Lexer::new(input.to_string());
+    let result = lexer.tokenize().expect("failed to tokenize");
+    let full_span = Span::compose(
+        expected.first().map_or(Span::new(0, 0), |t| t.span),
+        expected.last().map_or(Span::new(0, 0), |t| t.span),
+    );
+    assert_eq!(result, expected);
+    assert_eq!(&input[full_span.start..full_span.end], input);
+}
+
+#[test]
+fn test_invalid_number() {
+    let program = "1abc";
+    let mut lexer = Lexer::new(program.to_string());
+    let error = lexer.tokenize().expect_err("expected error");
+    assert_eq!(error, Error::invalid_number("1abc", Span::with_len(0, 4)));
+}
+
+#[test]
+fn test_empty_program() {
+    let program = "";
+    test_lexer(program, vec![]);
+}
+
+#[test]
+fn test_single_token() {
+    let program = "fn";
+    test_lexer(program, vec![Token::with_len(TokenKind::Fn, 0, 2)]);
+}
+
+#[test]
+fn test_unexpected_symbol() {
+    let program = "fn main() { let a = 1; return a + 3; } $";
+    let mut lexer = Lexer::new(program.to_string());
+    let error = lexer.tokenize().expect_err("expected error");
+    assert_eq!(error, Error::unexpected_symbol('$', 39));
+}
+
+#[test]
+fn test_program() {
+    let program = "fn main() { let a = 1; return a + 3; }";
+    test_lexer(
+        program,
+        vec![
+            Token::with_len(TokenKind::Fn, 0, 2),
+            Token::with_len(TokenKind::Identifier("main".to_string()), 3, 4),
+            Token::with_len(TokenKind::ParenOpen, 7, 1),
+            Token::with_len(TokenKind::ParenClose, 8, 1),
+            Token::with_len(TokenKind::BraceOpen, 10, 1),
+            Token::with_len(TokenKind::Let, 12, 3),
+            Token::with_len(TokenKind::Identifier("a".to_string()), 16, 1),
+            Token::with_len(TokenKind::Equal, 18, 1),
+            Token::with_len(TokenKind::Integer(1), 20, 1),
+            Token::with_len(TokenKind::Semicolon, 21, 1),
+            Token::with_len(TokenKind::Return, 23, 6),
+            Token::with_len(TokenKind::Identifier("a".to_string()), 30, 1),
+            Token::with_len(TokenKind::Plus, 32, 1),
+            Token::with_len(TokenKind::Integer(3), 34, 1),
+            Token::with_len(TokenKind::Semicolon, 35, 1),
+            Token::with_len(TokenKind::BraceClose, 37, 1),
+        ],
+    );
+}
